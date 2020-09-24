0643d9d6fdc0e5ad5ac5bddfc80a25f4683addca
Alexander Sjögren <alle656@hotmail.com>
2020-09-24 10:01:48 +0200

    Klar går att ändra delen om mig via json

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 let githubData;
 
 fetch('https://api.github.com/users/Alexander-sjogren/repos')
@@ -5,8 +6,17 @@ fetch('https://api.github.com/users/Alexander-sjogren/repos')
 .then(data => githubData = data)
 .then(json => {
     //console.log(githubData)
-    for (i=0; i<githubData.length; i++) {
+    for (let i=0; i<githubData.length; i++) {
         document.getElementById("githubul").insertAdjacentHTML("beforeend", "<li class='list-group-item'><a href='"+ githubData[i].html_url +"' target='_blank'>"+ githubData[i].name +"</a></li>");
     }  
 })
 
+fetch('aboutme.JSON')
+.then(response => response.json())
+.then (json => {
+   // console.log(json)
+    for (let i = 0; i < json.length; i++) {
+        console.log(json[i].myString);
+        document.getElementById("list1").insertAdjacentHTML("afterbegin", "<li class='list-group-item'>"+json[i].myString+"</li>")
+    }
+})
